ad05d7e9ff8c4e835152a05432aba1e31cfdd3c3
Fabio Morais <fabio.lmorais99@gmail.com>
2024-10-01 20:35:01 -0300

    Projeto criado e configurado o banco H2

---
+++ oakteste-backend/src/test/java/com/fabiomorais/oakteste/OaktesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiomorais.oakteste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaktesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
